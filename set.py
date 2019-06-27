aaf6ff2764253a093e78215057bf1478a5d48d63
d-shin <d-shin@bitcash.co.jp>
2019-06-27 17:12:15 +0900

    update11

--- set.py
+++ set.py
@@ -9,12 +9,20 @@ def checkTrait(c):
 text = open('name_db', encoding='utf-8', errors='ignore')
 
 items = text.readlines()
+items = [item.replace('\n' , '') for item in items]
 text.close()
 
 merged = set(items)
+result_list = []
+
+for item in merged:
+    if len(item) >= 4 or len(item) == 1:
+        continue
+    else:
+        result_list.append("{}\n".format(item))
 
 writer = open('name_db', mode='w', encoding='utf-8', errors='ignore')
 
-writer.writelines(merged)
+writer.writelines(result_list)
 
 writer.close()
